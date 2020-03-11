63b95cb92698f276e1f10c1e54018e64bc025e5a
Tae <tm@Taes-air.com>
2020-03-10 22:02:57 -0500

    Red Light |

--- othello/create.py
+++ othello/create.py
@@ -1,3 +1,6 @@
 def _create(light):
-    result = {'status': 'error: Above bound light integer'}
+    if (light > 9):
+        result = {'status': 'error: Above bound light integer'}
+    if (light < 0):
+        result = {'status': 'error: Below bound light integer'}
     return result
