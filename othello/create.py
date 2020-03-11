73e2a10a5616beda1224bee82066afb509c56a74
Tae <tm@Taes-air.com>
2020-03-10 22:20:53 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -1,6 +1,8 @@
 def _create(light = None):
     if (isinstance(light, str)):
         result = {'status':'error: Non integer light'}
+    elif (light == None):
+        result = {'status':'error: Null light'}
     elif (light > 9):
         result = {'status': 'error: Above bound light integer'}
     elif (light < 0):
