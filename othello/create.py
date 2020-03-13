0fdde6a4856fac69b1e3fc17be3cb45f847b7931
Tae <tm@Taes-air.com>
2020-03-12 22:01:07 -0500

    Green Light |

--- othello/create.py
+++ othello/create.py
@@ -3,5 +3,7 @@ def _create(parms):
         result = {'status': 'error: above bound light value'}
     if (int(parms['light']) < 0):
         result = {'status': 'error: below bound light value'}
+    if (isinstance(int(parms['light']), int)):
+        result = {'status': 'error: non-integer light value'}
         
     return result
