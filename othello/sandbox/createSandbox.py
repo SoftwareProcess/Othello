3b4c48ed85f11930237d5983cfbbc21074993f34
Tae <tm@Taes-air.com>
2020-03-14 18:12:49 -0500

    Red Light |

--- othello/sandbox/createSandbox.py
+++ othello/sandbox/createSandbox.py
@@ -5,22 +5,28 @@ Created on Mar 12, 2020
 '''
 
 parms = {'op': 'create', 'light': '1', 'dark': '2', 'blank': '0', 'size': '8'}
-parms['light'] = 'w'
+# parms['light'] = 'w'
+# 
+# try: (int(parms['light']))
+# except ValueError:
+#     print('caught it')
+#     
+# parms['light'] = '1'
+# print(parms)
+# 
+# result = {'board': 0, 
+#           'tokens': {'light': int(parms['light']), 'dark': int(parms['dark']),
+#                      'blank': int(parms['blank'])
+#                      },
+#           'status': 'ok',
+#           'integrity': ''}
+# del parms['light']
+# if 'light' not in parms.keys():
+#     parms['light'] = 1
+# print (parms)
+b = {'board': [int(parms['blank'])] * (int(parms['size']) ** 2)}
+if len(b.get('board')) == 64:
+    a =  b['board']
+    a[2] = 3
 
-try: (int(parms['light']))
-except ValueError:
-    print('caught it')
-    
-parms['light'] = '1'
-print(parms)
-
-result = {'board': 0, 
-          'tokens': {'light': int(parms['light']), 'dark': int(parms['dark']),
-                     'blank': int(parms['blank'])
-                     },
-          'status': 'ok',
-          'integrity': ''}
-del parms['light']
-if 'light' not in parms.keys():
-    parms['light'] = 1
-print (parms)
+print(b.get('board'))
