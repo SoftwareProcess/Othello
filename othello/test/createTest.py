a39f3bd614cbc7214a0f5bb38db70baf019ae02e
Tae <tm@Taes-air.com>
2020-03-14 22:49:42 -0500

    Yellow Light |

--- othello/test/createTest.py
+++ othello/test/createTest.py
@@ -481,6 +481,21 @@ class CreateTest(TestCase):
         self.actual = create(self.parms)
         self.assertEqual(expected, self.actual)
          
+    def test060_AllDefaultParameter(self):
+        self.setUp()
+        expected = {'board': [0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 1, 2, 0, 0, 0,
+                              0, 0, 0, 2, 1, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0],
+                    'tokens': {'light': 1, 'dark': 2, 'blank': 0},
+                    'status': 'ok',
+                    'integrity': 'b11fcf5f9ac9d3b8cea8085208e210182a8d6b73a84028562ab2c87d190b9ada'}
+        self.actual = create(self.parms)
+        self.assertEqual(expected, self.actual)
     #900 Sad Path
     def test900_AboveBoundLight(self):
         self.setUp()
