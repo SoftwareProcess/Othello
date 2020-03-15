c9f31b0449eee717bd6180aedf7e9934dfbdc34b
Tae <tm@Taes-air.com>
2020-03-15 02:13:09 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -5,6 +5,38 @@
 '''
 import hashlib
 
+def __checkBoundary(input):
+    for parameter in ('light', 'dark', 'blank'):
+        if (parameter == None):
+            return {'status': 'error: Null ' + parameter +  ' value'}
+        try: int(input(parameter))
+        except ValueError:
+            return {'status': 'error: non-integer ' + parameter + 'value'}
+        if (int(input(parameter)) > 9):
+            return {'status': 'error: above bound' + parameter + 'value'} 
+        elif (int(input(parameter)) < 0):
+            return {'status': 'error: below bound' + parameter + 'value'}
+        
+    if (input['size'] == None):
+        return {'status': 'error: Null size value'}
+    try: (int(input['size']))
+    except ValueError:
+        return {'status': 'error: non-integer size value'}
+    if (int(input['size']) > 16):
+        return {'status': 'error: above bound size value'}
+    elif (int(input['size']) < 6):
+        return {'status': 'error: below bound size value'}
+    elif (int(input['size']) % 2 != 0):
+        return {'status': 'error: Odd size value'}
+    
+    if (int(input['light']) == int(input['dark'])):
+        return {'status': 'error: light is equal to dark value'}
+    if (int(input['light']) == int(input['blank'])):
+        return {'status': 'error: blank is equal to light value'}
+    if (int(input['dark']) == int(input['blank'])):
+        return {'status': 'error: dark is equal to blank value'}
+    
+            
 def __setBoard(boardDict):
     board = boardDict['board']
     light = int(boardDict['tokens']['light'])
@@ -42,7 +74,6 @@ def __setBoard(boardDict):
         board[135] = dark
         board[136] = light
 
-
 def _create(parms):
     if ('light' not in parms.keys()):
         parms['light'] = 1
@@ -53,59 +84,7 @@ def _create(parms):
     if ('size' not in parms.keys()):
         parms['size'] = 8
         
-    # Light sad path boundary check        
-    if (parms['light'] == None):
-        return {'status': 'error: Null light value'}
-    try: (int(parms['light']))
-    except ValueError:
-        return {'status': 'error: non-integer light value'}
-    if (int(parms['light']) > 9):
-        return {'status': 'error: above bound light value'}
-    elif (int(parms['light']) < 0):
-        return {'status': 'error: below bound light value'}
-    
-    # dark sad path boundary check
-    if (parms['dark'] == None):
-        return {'status': 'error: Null dark value'}
-    try: (int(parms['dark']))
-    except ValueError:
-        return {'status': 'error: non-integer dark value'}
-    if (int(parms['dark']) > 9):
-        return {'status': 'error: above bound dark value'}
-    elif (int(parms['dark']) < 0):
-        return {'status': 'error: below bound dark value'}
-    
-    # blank sad path boundary check
-    if (parms['blank'] == None):
-        return {'status': 'error: Null blank value'}
-    try: (int(parms['blank']))
-    except ValueError:
-        return {'status': 'error: non-integer blank value'}
-    if (int(parms['blank']) > 9):
-        return {'status': 'error: above bound blank value'}
-    elif (int(parms['blank']) < 0):
-        return {'status': 'error: below bound blank value'}
-    
-    # size sad path boundary check
-    if (parms['size'] == None):
-        return {'status': 'error: Null size value'}
-    try: (int(parms['size']))
-    except ValueError:
-        return {'status': 'error: non-integer size value'}
-    if (int(parms['size']) > 16):
-        return {'status': 'error: above bound size value'}
-    elif (int(parms['size']) < 6):
-        return {'status': 'error: below bound size value'}
-    elif (int(parms['size']) % 2 != 0):
-        return {'status': 'error: Odd size value'}
-    
-    # Checking if values are equal to two different keys
-    if (int(parms['light']) == int(parms['dark'])):
-        return {'status': 'error: light is equal to dark value'}
-    if (int(parms['light']) == int(parms['blank'])):
-        return {'status': 'error: blank is equal to light value'}
-    if (int(parms['dark']) == int(parms['blank'])):
-        return {'status': 'error: dark is equal to blank value'}
+    __checkBoundary(parms)
     
     result = {'board': [int(parms['blank'])] * (int(parms['size']) ** 2), 
               'tokens': {'light': int(parms['light']), 'dark': int(parms['dark']),
