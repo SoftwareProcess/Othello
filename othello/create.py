ca484fe8fb9ceedf61c720031db64355a9f52808
Tae <tm@Taes-air.com>
2020-03-15 01:38:05 -0500

    Refactor |

--- othello/create.py
+++ othello/create.py
@@ -5,6 +5,44 @@
 '''
 import hashlib
 
+def __setBoard(boardDict):
+    board = boardDict['board']
+    light = int(boardDict['token']['light'])
+    dark = int(boardDict['token']['dark'])
+    numberOfElements = len(board)
+    
+    if (numberOfElements == 32):
+        board[14] = light
+        board[15] = dark
+        board[20] = dark
+        board[21] = light
+    if (numberOfElements == 64):
+        board[27] = light
+        board[28] = dark
+        board[35] = dark
+        board[36] = light
+    if (numberOfElements == 100):
+        board[44] = light
+        board[45] = dark
+        board[54] = dark
+        board[55] = light
+    if (numberOfElements == 144):
+        board[65] = light
+        board[66] = dark
+        board[77] = dark
+        board[78] = light
+    if (numberOfElements == 196):
+        board[90] = light
+        board[91] = dark
+        board[104] = dark
+        board[105] = light
+    if (numberOfElements == 256):
+        board[119] = light
+        board[120] = dark
+        board[135] = dark
+        board[136] = light
+
+
 def _create(parms):
     if ('light' not in parms.keys()):
         parms['light'] = 1
@@ -77,37 +115,7 @@ def _create(parms):
               'integrity': ''}
     
     # Setting up the board according to size
-    boardList = result['board']
-    if (len(result.get('board')) == 36):
-        boardList[14] = int(parms['light'])
-        boardList[15] = int(parms['dark'])
-        boardList[20] = int(parms['dark'])
-        boardList[21] = int(parms['light'])
-    if (len(result.get('board')) == 64):
-        boardList[27] = int(parms['light'])
-        boardList[28] = int(parms['dark'])
-        boardList[35] = int(parms['dark'])
-        boardList[36] = int(parms['light'])
-    if (len(result.get('board')) == 100):
-        boardList[44] = int(parms['light'])
-        boardList[45] = int(parms['dark'])
-        boardList[54] = int(parms['dark'])
-        boardList[55] = int(parms['light'])
-    if (len(result.get('board')) == 144):
-        boardList[65] = int(parms['light'])
-        boardList[66] = int(parms['dark'])
-        boardList[77] = int(parms['dark'])
-        boardList[78] = int(parms['light'])
-    if (len(result.get('board')) == 196):
-        boardList[90] = int(parms['light'])
-        boardList[91] = int(parms['dark'])
-        boardList[104] = int(parms['dark'])
-        boardList[105] = int(parms['light'])
-    if (len(result.get('board')) == 256):
-        boardList[119] = int(parms['light'])
-        boardList[120] = int(parms['dark'])
-        boardList[135] = int(parms['dark'])
-        boardList[136] = int(parms['light'])
+    __setBoard(result)
 
     # Sha256 Hex digest
     light = result['tokens']['light']
