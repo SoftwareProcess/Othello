5f86b614716b9d225d8564bdac34fd894ba54157
Tae <tm@Taes-air.com>
2020-03-29 08:14:28 -0500

    Green Light |

--- othello/status.py
+++ othello/status.py
@@ -15,9 +15,9 @@ def __checkParms(parmsIn):
         return {'status': 'error: null board'}
     if len(parmsIn.get('board')) not in (36, 64, 100, 144, 196, 256):
         return {'status': 'error: uneven board'}
-    for i in parmsIn.get('board'):
-        if i not in (int(parmsIn.get('light')), int(parmsIn.get('dark')), int(parmsIn.get('blank'))):
-            return {'status': 'error: incorrect board value'}   
+    if any(i in parmsIn.get('board') for i in [int(parmsIn.get('light')),
+            int(parmsIn.get('dark')), int(parmsIn.get('blank'))]):
+        return {'status': 'error: incorrect board value'}
     
     # Check integrity parameter
     if 'integrity' not in parmsIn.keys():
