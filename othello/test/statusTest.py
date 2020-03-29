c570f23b64527e97a7d5d2655c5f0cdd9d00c906
Tae <tm@Taes-air.com>
2020-03-29 06:28:20 -0500

    Green Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -187,7 +187,7 @@ class StatusTest(TestCase):
     
     def test942_NonHexIntegrity(self):
         self.setUp()
-        self.parms['integrity']) = 'f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465$'
+        self.parms['integrity'] = 'f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465$'
         expected = 'error: non hex characters'
         self.actual = status(self.parms)
         self.assertEqual(expected, self.actual['status'])
