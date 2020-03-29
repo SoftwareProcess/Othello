176934668ab064096d84a4e2b7668192a4d221b3
Tae <tm@Taes-air.com>
2020-03-29 02:59:41 -0500

    Green Light |

--- othello/status.py
+++ othello/status.py
@@ -8,6 +8,9 @@ from othello.create import _create as create
 def _status(parms):
     statusParmsIn = parms
     createOutput = create(statusParmsIn)
+    try: parms.get('board')
+    except KeyError: 
+        return {'status': 'error: board does not exist'}
     if parms.get('board') == None:
         return {'status': 'error: null board'}
     if len(parms.get('board')) not in (36, 64, 100, 144, 196, 256):
