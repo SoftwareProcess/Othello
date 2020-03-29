9963598146c7c5f00c7e58ac4290e565111e253e
Tae <tm@Taes-air.com>
2020-03-29 05:40:27 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -184,3 +184,14 @@ class StatusTest(TestCase):
         self.actual = status(self.parms)
         self.assertEqual(expected, self.actual['status'])
         self.tearDown()
+    
+    def test942_NonHexIntegrity(self):
+        self.setUp(
+        self.parms['integrity']) = 'f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465$'
+        expected = 'error: non hex characters'
+        self.actual = status(self.parms)
+        self.assertEqual(expected, self.actual['status'])
+        self.tearDown()
+            
+            
+            
