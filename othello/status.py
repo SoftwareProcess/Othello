ea7694208f7afcbab50071ab62dec9e765bf0145
Tae <tm@Taes-air.com>
2020-03-28 23:15:26 -0500

    Red Light |

--- othello/status.py
+++ othello/status.py
@@ -1,3 +1,8 @@
+'''
+    Created on Mar 27, 2020
+    
+    @author:    Tae Myles
+'''
 def _status(parms):
     result = {'status': 'status stub'}
     return result
---
+++ othello/test/statusTest.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 27, 2020
+
+@author:    Tae Myles
+'''
+from unittest import TestCase
+from othello.status import _status as status
+
+
+class Test(TestCase):
+    def setUp(self):
+        self.parms = {'op': 'status', 'light': '1', 'dark': '2',
+            'board': '[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0',
+            'integrity': '6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b'}
+            
+    def tearDown(self):
+        self.parms = {'op': 'status', 'light': '1', 'dark': '2',
+            'board': '[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0',
+            'integrity': '6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b'}
+
+    def test900_AboveBoundLight(self):
+        self.setUp()
+        self.parms['light'] = '10'
+        expected = 'error: above bound light value in board'
+        self.actual = status(self.parms)
+        self.assertEqual(expected, self.actual['status'])
+        self.tearDown()
