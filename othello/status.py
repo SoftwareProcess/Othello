8e8d65303b75bb683f1616107348292d7285a860
Tae <tm@Taes-air.com>
2020-03-29 01:30:06 -0500

    Green Light |

--- othello/status.py
+++ othello/status.py
@@ -8,5 +8,7 @@ from othello.create import _create as create
 def _status(parms):
     statusParmsIn = parms
     result = create(statusParmsIn)
+    if len(result['board']) not in (36, 64, 100, 144, 196, 256):
+        return {'status': 'error: non-square board'}
     return result
 
