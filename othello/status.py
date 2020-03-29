126bf00c73077c93d12fcf20fb033a2d733cdb6b
Tae <tm@Taes-air.com>
2020-03-29 02:50:18 -0500

    Green Light |

--- othello/status.py
+++ othello/status.py
@@ -8,6 +8,8 @@ from othello.create import _create as create
 def _status(parms):
     statusParmsIn = parms
     createOutput = create(statusParmsIn)
+    if len(parms.get('board')) == None:
+        return {'status': 'error: board does not exist'}
     if "board" in createOutput:
         if len(parms.get('board')) not in (36, 64, 100, 144, 196, 256):
             return {'status': 'error: uneven board'}
