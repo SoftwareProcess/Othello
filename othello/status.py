df5fb1d15f43e8cde6152d035db483aeffef2e94
Tae <tm@Taes-air.com>
2020-03-29 05:28:11 -0500

    Red Light |

--- othello/status.py
+++ othello/status.py
@@ -16,7 +16,7 @@ def __checkParms(parmsIn):
     # Check integrity parameter
     if len(parmsIn.get('integrity')) < 64:
         return {'status': 'error: short integrity'}
-    return createOutput['status']
+    return createOutput
 
 def _status(parms):
     statusParmsIn = parms
