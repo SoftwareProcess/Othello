867204262401bc208a8737a58feb7659c2adb90e
Tae <tm@Taes-air.com>
2020-03-31 06:32:48 -0500

    Yellow Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -137,6 +137,23 @@ class StatusTest(TestCase):
         self.actual = status(self.parms)
         self.assertEqual(expected, self.actual['status'])
         self.tearDown()
+        
+    def test040_LowBoundBlankNominalLightDarkBoardIntegrity(self):
+        self.setUp()
+        self.parms['light'] = '5'
+        self.parms['dark'] = '6'
+        self.parms['blank'] = '0'
+        self.parms['board'] = [9, 9, 9, 9, 9, 9,
+                               9, 9, 9, 9, 9, 9,
+                               9, 9, 5, 6, 9, 9, 
+                               9, 9, 6, 5, 9, 9, 
+                               9, 9, 9, 9, 9, 9, 
+                               9, 9, 9, 9, 9, 9]
+        self.parms['integrity'] = '062f219e852404144cd7967bcbac5d5d82c151697d8eacfd8c29779acbc58b19'
+        expected = 'ok'
+        self.actual = status(self.parms)
+        self.assertEqual(expected, self.actual['status'])
+        self.tearDown()
 
     # Sad path
     def test900_AboveBoundLight(self):
