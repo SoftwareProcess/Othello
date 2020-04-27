a5fe2b22e736028a1d8d06bf47e522b19bf79fe4
Tae <tm@Taes-air.com>
2020-04-26 22:00:56 -0500

    Green Light |

--- othello/place.py
+++ othello/place.py
@@ -17,8 +17,7 @@ def __validateTokenBoundaryAndType(tokenParmsIn):
     return tokenParmsIn
    
 def __validateLocation(locationParmsIn):
-    try: locationParmsIn.get('location')
-    except KeyError:
+    if 'location' not in locationParmsIn.keys()
         return {'status': 'error: missing location'}
     locationList = locationParmsIn['location'].split(':')
     if ':' not in locationParmsIn['location']:
